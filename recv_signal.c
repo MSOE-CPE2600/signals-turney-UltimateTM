0b13db7478df6dfe3eb60b1469a1cb31244cf171
Gabe Limberg <gabriellimberg@gmail.com>
2025-11-10 16:00:40 -0600

    Finished recv_signal and send_signal

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,44 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Gabe Limberg
  * 
  * Brief summary of program:
+ * Handles a signal from SIUSR1 and returns the PID of the sender and receiver
  */
+
+#define _POSIX_C_SOURCE 200809L // need to include for definitions of sig_action
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void handle_usr1(int signo, siginfo_t *info, void *ctx) {
+    if (signo == SIGUSR1) {
+        siginfo_t sig = *info;      
+        printf("Receiver PID: %d Sender PID: %d Value: %d\n", getpid(), sig.si_pid, sig.si_value.sival_int);
+        fflush(stdout);
+    }
+}
+
+int main(void) {
+    struct sigaction sa;
+    sa.sa_sigaction = handle_usr1;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+
+    // Register the SIGUSR1 handler
+    sigaction(SIGUSR1, &sa, NULL);
+    // Command to send SIGUSR1 to this process:
+    // kill -USR1 <PID>
+
+    printf("Process waiting for SIGUSR1 (PID: %d)\n", getpid());
+
+    // Wait indefinitely for incoming signals
+    while (1) {
+        pause();
+    }
+
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,39 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Gabe Limberg
  * 
  * Brief summary of program:
+ * Sends SIGUSR1 to the given PID using sigqueue, attaching a random int.
  */
+
+#define _POSIX_C_SOURCE 200809L // need to include for definitions of sig_action
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("No pid presented\n");
+        return 1;
+    }
+
+    pid_t target = (pid_t)atoi(argv[1]); // pid to be targeted
+    srand((unsigned)time(NULL)); // generating random time 
+    int rnd = rand();  // assigning random number to rnd
+
+    union sigval sv; // union for use in sigqueue
+    sv.sival_int = rnd; // attaching paylod to union
+
+    printf("Sender PID: %d  Sending value: %d to PID: %d\n", getpid(), rnd, target);
+
+    if (sigqueue(target, SIGUSR1, sv) == -1) {
+        printf("Failed to send\n");
+        return 1;
+    }
+
+    return 0;
+}
