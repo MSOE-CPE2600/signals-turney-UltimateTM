ed1a03fad73ef1a2e9d509577730bad5fe8c19c0
Gabe Limberg <gabriellimberg@gmail.com>
2025-11-10 15:06:26 -0600

    Finished segfault

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,21 +4,33 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Gabe Limberg
  * 
  * Brief summary of modifications:
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handle_fault(int fault) { // handler for fault 
+    if (fault == SIGSEGV) {
+        printf("Segementation fault detected.\n");
+    }
+}
 
 int main (int argc, char* argv[]) {
     // Declare a null pointer
     int* i = NULL;
 
+    signal(SIGSEGV, handle_fault); // SIGSEGV is detected and is handled at handle_fault
+
     // Dereference the null pointer
     printf("The value of i is: %d\n", *i);
 
-    // Return to exit the program
+    // Return to exit the program, *is never reached due to the segmentation fault*
     return 0;
 }
